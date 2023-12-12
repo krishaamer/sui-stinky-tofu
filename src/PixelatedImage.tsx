beaa3f291d73b0b12f0d35692d4d66b6d787bbe5
Kris Haamer <kris@haam.co>
2023-12-12 15:41:37 +0800

    TS issues

--- src/PixelatedImage.tsx
+++ src/PixelatedImage.tsx
@@ -1,14 +1,20 @@
-import React, { useRef, useEffect, useState } from "react";
+import { useRef, useEffect, useState, FC } from "react";
 import p5 from "p5";
 
-const PixelatedImage = ({ src, loadingTime }) => {
-  const sketchRef = useRef();
-  const [loading, setLoading] = useState(true);
-  let img,
-    pixelSize = 20,
-    interval;
+// Define a type for the component's props
+type PixelatedImageProps = {
+  src: string;
+  loadingTime: number;
+};
 
-  const sketch = (p) => {
+const PixelatedImage: FC<PixelatedImageProps> = ({ src, loadingTime }) => {
+  const sketchRef = useRef<HTMLDivElement>(null); // Specify the type for the ref
+  const [loading, setLoading] = useState<boolean>(true); // Specify type for state variable
+  let img: p5.Image;
+  let pixelSize: number = 20;
+  let interval: NodeJS.Timer;
+
+  const sketch = (p: p5) => {
     p.preload = () => {
       img = p.loadImage(src, () => setLoading(false));
     };
@@ -17,24 +23,23 @@ const PixelatedImage = ({ src, loadingTime }) => {
       const canvas = p.createCanvas(400, 400);
       canvas.style("width", "400px");
       canvas.style("height", "400px");
+      canvas.style("border-radius", "10px");
       img.resize(400, 400);
     };
 
     p.draw = () => {
       if (loading) {
-        // Draw pixelated image while loading
         for (let y = 0; y < p.height; y += pixelSize) {
           for (let x = 0; x < p.width; x += pixelSize) {
-            let i = (y * p.width + x) * 4;
-            let r = img.pixels[i];
-            let g = img.pixels[i + 1];
-            let b = img.pixels[i + 2];
+            const i = (y * p.width + x) * 4;
+            const r = img.pixels[i];
+            const g = img.pixels[i + 1];
+            const b = img.pixels[i + 2];
             p.noStroke();
             p.fill(r, g, b);
             p.rect(x, y, pixelSize, pixelSize);
           }
         }
-
         img.loadPixels();
       } else {
         // Draw original image once loading is complete
@@ -44,20 +49,22 @@ const PixelatedImage = ({ src, loadingTime }) => {
   };
 
   useEffect(() => {
-    const p5Instance = new p5(sketch, sketchRef.current);
+    if (sketchRef.current) {
+      const p5Instance = new p5(sketch, sketchRef.current);
 
-    interval = setInterval(() => {
-      pixelSize = Math.max(1, pixelSize - 1);
-      if (pixelSize === 1) {
+      interval = setInterval(() => {
+        pixelSize = Math.max(1, pixelSize - 1);
+        if (pixelSize === 1) {
+          clearInterval(interval);
+          setLoading(false); // Set loading to false when pixelation is done
+        }
+      }, loadingTime / 20);
+
+      return () => {
         clearInterval(interval);
-        setLoading(false); // Set loading to false when pixelation is done
-      }
-    }, loadingTime / 20);
-
-    return () => {
-      clearInterval(interval);
-      p5Instance.remove();
-    };
+        p5Instance.remove();
+      };
+    }
   }, [src, loadingTime]);
 
   return <div ref={sketchRef}></div>;
